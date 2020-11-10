720113018b5b62329a1233cbe6c56e871e916955
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-11-10 17:47:24 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = ">= 0.12.4"
+      source  = "philips-labs/cloudfoundry"
+      version = ">= 0.1206.0"
     }
   }
 }
